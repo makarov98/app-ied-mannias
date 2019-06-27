5d23e7d1e3a6b72a1b52f8c644105b9ae947870b
IED Student <>
2019-06-27 09:52:14 +0200

    Aggiunta schermata carrello, aggiustato lo script meteo e icona target

---
+++ UmarellApp/Models/Meteo.swift
@@ -0,0 +1,17 @@
+//
+//  Meteo.swift
+//  UmarellApp
+//
+//  Created by IED Student on 27/06/2019.
+//  Copyright © 2019 IED. All rights reserved.
+//
+
+import UIKit
+
+class Meteo {
+    
+    var temperatura : Double?
+    var descrizione : String?
+    
+    
+}
